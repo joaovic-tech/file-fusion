a6bed9d4323b94ecfdc1b549984fb7f057c2056e
joaovic <joaovictorca2004@gmail.com>
2024-08-06 21:25:36 -0300

    refatorando o `backend` e armazenamento dos arquivos
    Alterando o endpoint de upload para processar arquivos PDF diretamente na memória usando BytesIO, eliminando a necessidade de armazenar arquivos temporários no servidor. Isso melhora a eficiência e a segurança ao evitar a escrita no disco. A resposta é retornada como um fluxo de dados, permitindo o download direto do PDF mesclado.

--- apps/server/main.py
+++ apps/server/main.py
@@ -1,14 +1,10 @@
 from fastapi import FastAPI, File, HTTPException, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import FileResponse
-from fastapi.staticfiles import StaticFiles
-import os
+from io import BytesIO
 from PyPDF2 import PdfMerger
+from fastapi.responses import StreamingResponse
 
 app = FastAPI()
-
-UPLOAD_DIR = "uploads"
-os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 # Configuração de CORS
 app.add_middleware(
@@ -16,16 +12,11 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "https://file-fusion-client.vercel.app",
-        "https://file-fusion-client-joaovictechs-projects.vercel.app",
-        "https://file-fusion-client-git-main-joaovictechs-projects.vercel.app",
-        "https://file-fusion-client-pvowbb1pw-joaovictechs-projects.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-app.mount("/static", StaticFiles(directory="uploads"), name="static")
 
 
 @app.get("/")
@@ -33,55 +24,39 @@ async def root():
     return {"message": "Api do FileFusion"}
 
 
-def merge_pdfs(input_folder, output_filename):
-    pdf_files = [file for file in os.listdir(input_folder) if file.endswith(".pdf")]
-    pdf_files.sort()
-
-    pdf_merger = PdfMerger()
-
-    for pdf_file in pdf_files:
-        pdf_merger.append(os.path.join(input_folder, pdf_file))
-
-    with open(output_filename, "wb") as output_file:
-        pdf_merger.write(output_file)
-
-    print(f"Arquivos PDF mesclados com sucesso em {output_filename}")
-
-    # Remover arquivos individuais
-    for pdf_file in pdf_files:
-        os.remove(os.path.join(input_folder, pdf_file))
-
-
 @app.post("/upload")
-async def upload_file(files: list[UploadFile] = File(...)):
+async def upload_pdf(files: list[UploadFile] = File(...)):
     try:
-        # Verifica se a lista de arquivos está vazia
-        if not files:
-            raise HTTPException(status_code=400, detail="Nenhum arquivo enviado.")
+        # Inicializa o PdfMerger
+        merger = PdfMerger()
 
-        # Salva os arquivos recebidos
         for file in files:
-            file_location = f"{UPLOAD_DIR}/{file.filename}"
-            with open(file_location, "wb+") as file_object:
-                file_object.write(file.file.read())
+            if file.content_type != "application/pdf":
+                raise HTTPException(
+                    status_code=400, detail="Todos os arquivos devem ser PDFs."
+                )
 
-        output_filename = f"{UPLOAD_DIR}/arquivo_mesclado.pdf"
+            contents = await file.read()
+            pdf_file = BytesIO(contents)
 
-        # Remove o arquivo de saída se já existir
-        if os.path.exists(output_filename):
-            os.remove(output_filename)
+            # Adiciona o arquivo PDF ao merger
+            merger.append(pdf_file)
 
-        # Mescla os PDFs
-        merge_pdfs(UPLOAD_DIR, output_filename)
+        # Cria um BytesIO para armazenar o PDF unido
+        merged_pdf = BytesIO()
+        merger.write(merged_pdf)
+        merged_pdf.seek(0)
 
-        return FileResponse(
-            output_filename,
+        # Retorna o PDF combinado como um arquivo para download
+        return StreamingResponse(
+            merged_pdf,
             media_type="application/pdf",
-            filename="arquivo_mesclado.pdf",
+            headers={"Content-Disposition": "attachment; filename=merged.pdf"},
         )
+
     except HTTPException as http_exc:
         # Retorna erros HTTP específicos
         raise http_exc
     except Exception as e:
         # Retorna um erro genérico para outras exceções
-        return {"error": f"Ocorreu um erro: {str(e)}"}
+        raise HTTPException(status_code=500, detail=f"Ocorreu um erro: {str(e)}")
