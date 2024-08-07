2eb6644f31131688f72474b99c22f5ac0ce0b128
joaovic <joaovictorca2004@gmail.com>
2024-08-06 21:33:39 -0300

    deixando o servidor livre para todos os acessos

--- apps/server/main.py
+++ apps/server/main.py
@@ -9,10 +9,7 @@ app = FastAPI()
 # Configuração de CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://file-fusion-client.vercel.app",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
